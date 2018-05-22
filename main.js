c01ce0a39ef82fca1b728af48e7713bfc8d4a74d
Desk <927332199@qq.com>
2018-05-23 01:15:35 +0800

    522

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 var app=document.getElementById('app');
 var scan=document.getElementById('scan');
-var menu1=document.getElementById('menu1');
-var menu2=document.getElementById('menu2');
-var menu3=document.getElementById('menu3');
-var menu4=document.getElementById('menu4');
-var menu5=document.getElementById('menu5');
-var menu6=document.getElementById('menu6');
-var menu7=document.getElementById('menu7');
-
 
 app.onmouseenter=function () {
     scan.style.display='block';
@@ -16,13 +8,43 @@ app.onmouseleave=function () {
     scan.style.display='none';
 }
 
-var item1=document.getElementsByClassName('item1')[0];
-
-menu1.onmouseenter=function () {
-    item1.style.display='block';
-    menu1.style.background='red'
+var items=document.getElementsByClassName('item');
+for (var i=0; i<items.length; i++){
+    !function (i) {
+        items[i].onmouseenter=function () {
+            items[i].style.background='#a7a7a7'
+        }
+        items[i].onmouseleave=function () {
+            items[i].style.background='#4b4b4b'
+        }
+    }(i)
 }
-menu1.onmouseleave=function () {
-    item1.style.display='none';
-    menu1.style.background='#4b4b4b'
+
+var index=0;
+setInterval(function() {
+    index++;
+    show(index%6);
+},2000);
+
+var pics=document.getElementById('banner').getElementsByTagName('img');
+var dots=document.getElementById('dots').getElementsByTagName('span');
+for(var i=0;i<6;i++) {
+    dots[i].id=i;//为按钮们赋id,当然也可以在html里赋值
+    dots[i].onclick=function () {
+        show(this.id)//传入被点击按钮的索引值,并显示相应的图片和按钮
+    };
+}
+
+function show(index) {
+    for(var i=0;i<6;i++){
+        pics[i].style.display='none';
+        dots[i].className='';
+    }
+    pics[index].style.display='block';
+    dots[index].className='active';
+}
+
+var goTop=document.getElementById('goTop');
+goTop.onclick=function back() {
+    document.documentElement.scrollTop=0;
 }
