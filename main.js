7a6c0b966a77e18f6010f034a7075611fef973bc
LG <2393146578@qq.com>
2018-05-28 14:59:45 +0800

    modify

--- main.js
+++ main.js
@@ -8,34 +8,39 @@ app.onmouseleave=function () {
     scan.style.display='none';
 }
 
-var seconds=document.getElementsByClassName('second');
+// 二级菜单触发事件
+var subMenus=document.getElementsByClassName('sub-menu');
 var items=document.getElementsByClassName('item');
+
 for (var i=0; i<items.length; i++){
+    // 用闭包实现
     !function (i) {
         items[i].onmouseenter=function () {
-            items[i].style.background='#a7a7a7'
-            seconds[i].style.display='block';
+            items[i].style.background='rgba(180,180,180,0.5)'
+            subMenus[i].style.display='block';
         }
         items[i].onmouseleave=function () {
-            items[i].style.background='#4b4b4b'
-            seconds[i].style.display='none';
+            items[i].style.background='#2b333b'
+            subMenus[i].style.display='none';
         }
     }(i)
 }
 
-
+// 轮播图
 var index=0;
 setInterval(function() {
     index++;
     show(index%6);
-},4000);
+    move(pics[index%6],-1100)
+},14000);
 
 var pics=document.getElementById('banner').getElementsByTagName('img');
 var dots=document.getElementById('dots').getElementsByTagName('span');
 for(var i=0;i<6;i++) {
     dots[i].id=i;//为按钮们赋id,当然也可以在html里赋值
     dots[i].onclick=function () {
-        show(this.id)//传入被点击按钮的索引值,并显示相应的图片和按钮
+        show(this.id)//传入被点击按钮的索引值,并显示相应的图片和按钮,
+        // 如果this.id用i则由于i始终为6因此异常，可以用闭包解决
     };
 }
 
@@ -48,6 +53,22 @@ function show(index) {
     dots[index].className='active';
 }
 
+
+function move(ele,target) {
+    clearInterval(ele.timer);
+    var speed=target>ele.offsetLeft?10:-10;
+
+    ele.timer=setInterval(function () {
+        var v=target-ele.offsetLeft;
+        ele.style.left=ele.offsetLeft+speed+'px';
+
+        if(Math.abs(v)<Math.abs(speed)){
+            ele.style.left=target+'px';
+            clearInterval(ele.timer);
+        }
+    })
+}
+// 返回顶部
 var goTop=document.getElementById('goTop');
 goTop.onclick=function back() {
     document.documentElement.scrollTop=0;
