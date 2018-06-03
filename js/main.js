ba9b8c5895c1fcd60d68dc32f16d7d0f0799cab0
LG <2393146578@qq.com>
2018-06-03 16:51:45 +0800

    signin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+var signin=document.getElementById('signInBtn');
+var signup=document.getElementById('signUpBtn');
+
+var signform=document.getElementById('signForm');
+var remind=document.getElementById('remind');
+
+signin.onclick=function () {
+    signform.style.display='block';
+}
+signup.onclick=function () {
+    scan.style.display='none';
+}
+
+function validate(num) {
+    if(num.length!=11){
+        remind.innerText='请输入正确的手机号'
+    }
+    var reg=/^(((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+    if(reg.test(num)){
+        remind.innerText='请输入正确的手机号'
+    }
+}
+
+
+//悬停触发下载App二维码弹出层
 var app=document.getElementById('app');
 var scan=document.getElementById('scan');
 
