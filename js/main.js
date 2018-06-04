ca6245c393cb3aca40ff98c7a03047ce7dcf62ce
interest1 <927332199@qq.com>
2018-06-04 21:10:28 +0800

    signIn

--- js/main.js
+++ js/main.js
@@ -1,23 +1,59 @@
-var signin=document.getElementById('signInBtn');
-var signup=document.getElementById('signUpBtn');
+var signInBtn=document.getElementById('signInBtn');
+var signUpBtn=document.getElementById('signUpBtn');
 
-var signform=document.getElementById('signForm');
-var remind=document.getElementById('remind');
-signin.onclick=function () {
+var signForm=document.getElementById('signForm');
+var backdrop=document.getElementById('backdrop');
 
-    signform.style.display='block';
+var headSignIn=document.getElementById('headSignIn');
+var headSignUp=document.getElementById('headSignUp');
+
+var remindUserName=document.getElementById('remindUserName');
+var remindPassWd=document.getElementById('remindPassWd');
+signInBtn.onclick=function () {
+    signForm.style.display='block';
+    backdrop.style.display='block';
+    headSignIn.style.color='red';
 }
-signup.onclick=function () {
-    scan.style.display='none';
+signUpBtn.onclick=function () {
+    signForm.style.display='block';
+    backdrop.style.display='block';
+    headSignUp.style.color='red';
 }
+
+backdrop.onclick=function () {
+    signForm.style.display='none';
+    backdrop.style.display='none';
+}
+headSignIn.onclick=function () {
+    headSignIn.style.color='red';
+    headSignUp.style.color='black';
+}
+headSignUp.onclick=function () {
+    headSignUp.style.color='red';
+    headSignIn.style.color='black';
+}
+
+
 function validate() {
-    var phone=document.getElementById('validatePhone').value;
-    if(phone.length!=11){
-        remind.innerText='请输入正确的手机号'
+    var username=document.getElementById('checkUserName').value;
+    var passwd=document.getElementById('checkPassWd').value;
+
+    var isPhone = /^[1][3,4,5,7,8][0-9]{9}$/;
+    var isEmail = /^\w+@[a-zA-Z0-9]{2,10}(?:\.[a-z]{2,4}){1,3}$/;
+    var isPasswd=  /^[A-Za-z0-9]{6,16}$/;
+    if(!isPhone.test(username) && !isEmail.test(username)){
+        remindUserName.innerText='请输入正确的手机号或邮箱';
+        remindUserName.style.color='red';
+    }else{
+        remindUserName.innerText='用户名有效'
+        remindUserName.style.color='red';
     }
-    var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
-    if(!myreg.test(phone)){
-        // remind.innerText='请输入正确的手机号'
+    if(!isPasswd.test(passwd)){
+        remindPassWd.innerText='请输入有效的密码';
+        remindPassWd.style.color='red';
+    }else{
+        remindPassWd.innerText='密码有效'
+        remindPassWd.style.color='red';
     }
 }
 
