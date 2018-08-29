38cbe0151bd4e7131033d1ac41902a7425af3a44
interest1 <927332199@qq.com>
2018-08-29 18:11:15 +0800

    style modification

--- js/alert_layer.js
+++ js/alert_layer.js
@@ -1,59 +1,67 @@
 var backdrop=$('#backdrop');
 var signForm=$('#signForm');
 
-$('#signInBtn, #signUpBtn').click(function () {
+//点击弹出登录框
+$('#signInBtn').click(function () {
     $('#signForm, #backdrop').show()
     $('#headSignIn').css('color','red');
     $('#headSignUp').css('color','black');
 })
+$('#signUpBtn').click(function () {
+    $('#signForm, #backdrop').show()
+    $('#headSignUp').css('color','red');
+    $('#headSignIn').css('color','black');
+})
 
-$('.icon-cancel, #backdrop').click(turnOffForm);
+$('.icon-cancel, #backdrop').click(shutDown);
 
-function turnOffForm() {
+function shutDown() {
     $('#signForm, #backdrop').hide();
 }
-headSignIn.onclick=function () {
-    headSignIn.style.color='red';
-    headSignUp.style.color='black';
-}
-headSignUp.onclick=function () {
-    headSignUp.style.color='red';
-    headSignIn.style.color='black';
-}
 
+//弹窗里的事件
+$('#headSignIn').click(function () {
+    $('#headSignIn').css('color','red');
+    $('#headSignUp').css('color','black');
+})
+$('#headSignUp').click(function () {
+    $('#headSignUp').css('color','red');
+    $('#headSignIn').css('color','black');
+})
+
+// 登录注册验证
 var login=document.getElementById('login');
-var username=document.getElementById('checkUserName');
+var username=document.getElementById('checkName');
 username.onblur=checkName;
 login.onclick=function () {
     checkPwd();
     if(checkPwd){
-        turnOffForm();
+        shutDown();
     }
 };
 
-
-var remindUserName=$('#remindUserName')[0];
-var remindPassWd=$('#remindPassWd')[0];
+var remindName=$('#remindName')[0];
+var remindPwd=$('#remindPwd')[0];
 
 function checkName() {
     var name=username.value;
     var isPhone = /^[1][3,4,5,7,8][0-9]{9}$/;
     var isEmail = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
     if(!isPhone.test(name) && !isEmail.test(name)){
-        remindUserName.innerText='请输入正确的手机号或邮箱';
-        remindUserName.style.color='red';
+        remindName.innerText='请输入正确的手机号或邮箱';
+        remindName.style.color='red';
     }else{
-        remindUserName.innerText='用户名有效'
-        remindUserName.style.color='red';
+        remindName.innerText='用户名有效'
+        remindName.style.color='red';
     }
 
 }
 function checkPwd() {
-    var passwd=document.getElementById('checkPassWd').value;
+    var passwd=document.getElementById('checkPwd').value;
     var isPasswd=  /^[A-Za-z0-9]{6,16}$/;
     if(!isPasswd.test(passwd)){
-        remindPassWd.innerText='请输入有效的密码';
-        remindPassWd.style.color='red';
+        remindPwd.innerText='请输入有效的密码';
+        remindPwd.style.color='red';
     }else{
         return true;
     }
@@ -61,7 +69,6 @@ function checkPwd() {
 
 
 //悬停触发下载App二维码弹出层
-
 $('#app').mouseenter(function () {
     $('#scan').show();
 })
